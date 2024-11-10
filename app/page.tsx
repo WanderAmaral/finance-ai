959544de34d5453ddb03d255d82c91191bf0b250
Wander Amaral <wanderamaral013@gmail.com>
2024-11-10 00:01:56 -0300

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
-export default function Home() {
-  
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-  return <h1 className="flex items-end px-2">Hello world</h1>;
+export default async function Home() {
+  const { userId } = await auth();
+
+  if (!userId) {
+    redirect("/login");
+  }
+
+  return (
+    <div className="flex h-full items-center justify-center">
+      <UserButton showName />
+    </div>
+  );
 }
