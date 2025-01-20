927812d1715cd1ea00622c0d414608234a8c8589
Wander Amaral <wanderamaral013@gmail.com>
2025-01-20 20:37:35 -0300

    commit

---
+++ app/(home)/page.tsx
@@ -0,0 +1,30 @@
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import SummaryCards from "./_components/summary-cards";
+import TimeSelect from "./_components/time-select";
+
+interface HomeProps {
+  searchParams: {
+    month: string;
+  };
+}
+
+export default async function Home({ searchParams: { month } }: HomeProps) {
+  const { userId } = await auth();
+
+  if (!userId) {
+    redirect("/login");
+  }
+
+  return (
+    <>
+      <div className="space-y-6 p-6">
+        <div className="flex justify-between">
+          <h1 className="text-2xl font-bold">Dashboard</h1>
+          <TimeSelect />
+        </div>
+        <SummaryCards month={month} />
+      </div>
+    </>
+  );
+}
