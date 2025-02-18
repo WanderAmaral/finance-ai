65781a62fd0ab40b53b92d11c160bc9a39308747
Wander Amaral <wanderamaral013@gmail.com>
2025-02-17 21:57:53 -0300

    commit

---
+++ app/subscription/_actions/create-checkout/index.ts
@@ -0,0 +1,41 @@
+"use server";
+
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import Stripe from "stripe";
+
+export const createCheckout = async () => {
+  const { userId } = await auth();
+
+  if (!userId) {
+    redirect("/login");
+  }
+
+  if (!process.env.STRIPE_SECRET_KEY) {
+    throw new Error("Missing Stripe secret key");
+  }
+
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+    apiVersion: "2024-10-28.acacia",
+  });
+
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    line_items: [
+      {
+        price: process.env.STRIPE_PREMIUM_PLAN_PRICE_ID,
+        quantity: 1,
+      },
+    ],
+    mode: "subscription",
+    success_url: `http://localhost:3000`,
+    cancel_url: `${process.env.NEXT_PUBLIC_BASE_URL}/subscription/cancel`,
+    subscription_data: {
+      metadata: {
+        clerk_user_id: userId,
+      },
+    },
+  });
+
+  return { sessionId: session.id };
+};
